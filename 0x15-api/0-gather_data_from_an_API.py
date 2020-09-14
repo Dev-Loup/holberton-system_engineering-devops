a4eb4310aef97f82f7dde6ee3510166616fee3ab
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-14 23:25:41 +0000

    [add]: changed indexed get to get method

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ import requests
 url_base = 'https://jsonplaceholder.typicode.com'
 emp_id = argv[1]
 req_emp = requests.get('{}/users/{}'.format(url_base, emp_id))
-emp_name = req_emp.json()['name']
+emp_name = req_emp.json().get('name')
 all_t = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
 done_t = requests.get('{}/todos?userId={}&completed=true'
                       .format(url_base, emp_id))
@@ -18,4 +18,4 @@ print('Employee {} is done with tasks({}/{}):'
       .format(emp_name, len(done_t.json()), len(all_t.json())))
 
 for task in done_t.json():
-    print('\t {}'.format(task['title']))
+    print('\t {}'.format(task.get('title')))
