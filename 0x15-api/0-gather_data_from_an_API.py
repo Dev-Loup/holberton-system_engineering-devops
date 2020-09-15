ecff5fb036c7509a804137a0c17e0c92b84c60bb
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-15 00:02:39 +0000

    [fix]: identation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,10 +13,10 @@ try:
     emp_name = req_emp.json().get('name')
     all_t = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
     done_t = requests.get('{}/todos?userId={}&completed=true'
-                        .format(url_base, emp_id))
+                          .format(url_base, emp_id))
 
     print('Employee {} is done with tasks({}/{}):'
-        .format(emp_name, len(done_t.json()), len(all_t.json())))
+          .format(emp_name, len(done_t.json()), len(all_t.json())))
 
     for task in done_t.json():
         print('\t {}'.format(task.get('title')))
