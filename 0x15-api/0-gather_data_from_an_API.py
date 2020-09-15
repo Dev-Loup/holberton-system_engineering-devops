c61a1cf50228e90d895d20ed3e171a202e3b84a8
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-15 00:01:25 +0000

    [add]: try except for valueerror

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,15 +7,18 @@ import requests
 from sys import argv
 
 url_base = 'https://jsonplaceholder.typicode.com'
-emp_id = argv[1]
-req_emp = requests.get('{}/users/{}'.format(url_base, emp_id))
-emp_name = req_emp.json().get('name')
-all_t = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
-done_t = requests.get('{}/todos?userId={}&completed=true'
-                      .format(url_base, emp_id))
+try:
+    emp_id = int(argv[1])
+    req_emp = requests.get('{}/users/{}'.format(url_base, emp_id))
+    emp_name = req_emp.json().get('name')
+    all_t = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
+    done_t = requests.get('{}/todos?userId={}&completed=true'
+                        .format(url_base, emp_id))
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(emp_name, len(done_t.json()), len(all_t.json())))
+    print('Employee {} is done with tasks({}/{}):'
+        .format(emp_name, len(done_t.json()), len(all_t.json())))
 
-for task in done_t.json():
-    print('\t {}'.format(task.get('title')))
+    for task in done_t.json():
+        print('\t {}'.format(task.get('title')))
+except ValueError:
+    exit
