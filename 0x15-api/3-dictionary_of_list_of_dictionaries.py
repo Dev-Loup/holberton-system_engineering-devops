e48dbd0933cddc0d4fe6c98c46a8b1984eb63cfe
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-15 02:56:13 +0000

    [add]: remove try

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,23 +11,20 @@ def todos_json_all():
     """
 
     url_base = 'https://jsonplaceholder.typicode.com'
-    try:
-        task_all = requests.get('{}/todos'.format(url_base))
+    task_all = requests.get('{}/todos'.format(url_base))
 
-        task_dict = {}
-        for task in task_all.json():
-            emp_id = task.get('userId')
-            if task_dict.get(emp_id) is None:
-                task_dict[emp_id] = []
-                emp_data = requests.get('{}/users/{}'.format(url_base, emp_id))
-                emp_name = emp_data.json().get('username')
-            task_dict[emp_id].append({'task': task.get('title'),
-                                      'completed': task.get('completed'),
-                                      'username': emp_name})
-        with open('todo_all_employees.json', 'w') as request:
-            json.dump(task_dict, request)
-    except ValueError:
-        exit
+    task_dict = {}
+    for task in task_all.json():
+        emp_id = task.get('userId')
+        if task_dict.get(emp_id) is None:
+            task_dict[emp_id] = []
+            emp_data = requests.get('{}/users/{}'.format(url_base, emp_id))
+            emp_name = emp_data.json().get('username')
+        task_dict[emp_id].append({'task': task.get('title'),
+                                  'completed': task.get('completed'),
+                                  'username': emp_name})
+    with open('todo_all_employees.json', 'w') as request:
+        json.dump(task_dict, request)
 
 
 if __name__ == '__main__':
