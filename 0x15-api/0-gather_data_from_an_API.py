fe17953cdf6320c64d45eae9605908dcb021596b
LoopingDiego <60360505+LoopingDiego@users.noreply.github.com>
2020-09-15 00:35:33 +0000

    [add]: add if name main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,19 +6,28 @@
 import requests
 from sys import argv
 
-url_base = 'https://jsonplaceholder.typicode.com'
-try:
-    emp_id = int(argv[1])
-    req_emp = requests.get('{}/users/{}'.format(url_base, emp_id))
-    emp_name = req_emp.json().get('name')
-    all_t = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
-    done_t = requests.get('{}/todos?userId={}&completed=true'
-                          .format(url_base, emp_id))
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(emp_name, len(done_t.json()), len(all_t.json())))
+def todos():
+    """ execute if main
+    """
 
-    for task in done_t.json():
-        print('\t {}'.format(task.get('title')))
-except ValueError:
-    exit
+    url_base = 'https://jsonplaceholder.typicode.com'
+    try:
+        emp_id = int(argv[1])
+        emp_data = requests.get('{}/users/{}'.format(url_base, emp_id))
+        emp_name = emp_data.json().get('name')
+        task_all = requests.get('{}/todos?userId={}'.format(url_base, emp_id))
+        task_done = requests.get('{}/todos?userId={}&completed=true'
+                                 .format(url_base, emp_id))
+
+        print('Employee {} is done with tasks({}/{}):'
+              .format(emp_name, len(task_done.json()), len(task_all.json())))
+
+        for task in task_done.json():
+            print('\t {}'.format(task.get('title')))
+    except ValueError:
+        exit
+
+
+if __name__ == '__main__':
+    todos()
